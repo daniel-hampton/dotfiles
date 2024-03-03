e53ef2e49ce61470806520058dddcbd763432427
Daniel Hampton <dhampton084@gmail.com>
2024-03-03 17:15:20 -0500

    remove nvim for fresh install

--- nvim/astronvim_install.sh
+++
@@ -1,8 +0,0 @@
-
-# If astronvim is not installed
-if [ ! -d ~/.config/nvim ]; then
-  mv ~/.local/share/nvim ~/.local/share/nvim.bak
-  mv ~/.local/state/nvim ~/.local/state/nvim.bak
-  mv ~/.cache/nvim ~/.cache/nvim.bak
-  git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
-fi
